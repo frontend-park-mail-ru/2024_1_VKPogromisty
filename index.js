37cc3e85d1493810a9f724439c2d0509fc34af5b
Famelbyby <lexagorbunov14@gmail.com>
2024-03-04 00:08:30 +0300

    VKP-30-server: добавить .gitignore и удалить node_modules

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 
 app.use(cors())
 app.use(express.static(path.resolve(__dirname, 'static')));
-app.use(express.static(path.resolve(__dirname, 'node_modules')));
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.json());
 app.use(body.json);
@@ -18,7 +17,7 @@ app.use(cookie());
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+}); 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
