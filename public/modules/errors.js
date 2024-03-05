099341452487756903773940bf8968f2fa9bfaf5
Famelbyby <lexagorbunov14@gmail.com>
2024-03-05 23:17:42 +0300

    VKP-29-validation: добавить файл с ошибками для удобного показа ошибок

---
+++ public/modules/errors.js
@@ -0,0 +1,8 @@
+export const errors = {
+    incorrectPasswordLength: "Пароль должен иметь длину не менее 6 символов",
+    passwordMismatch: "Пароли должны совпадать",
+    incorrectEmail: "Некорректная почта",
+    incorrectName: "Некорретное имя",
+    impossibleDate: "Некорректная дата",
+    incorrectPassword: "Некорректный пароль",
+};
