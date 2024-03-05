87af4a24a6fc677a285ed845300b405c89e0489e
Famelbyby <lexagorbunov14@gmail.com>
2024-03-06 00:27:28 +0300

    VKP-29-validation: добавить функцию responses для шаблона возврата из services.js

--- public/modules/consts.js
+++ public/modules/consts.js
@@ -1,1 +1,9 @@
 export const API_URL = "http://94.139.246.134:8080/api/v1";
+
+export const responses = (ok, body, error) => {
+    return {
+        ok: ok,
+        body: body,
+        error: error,
+    }
+}
