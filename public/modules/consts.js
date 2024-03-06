5b5ce40df7a6030e986ccdd2587e8fb9fe0f2279
PetrMitin <petr09mitin@mail.ru>
2024-03-06 14:40:18 +0300

    VKP-29-validation: Добавить ESLint config

--- public/modules/consts.js
+++ public/modules/consts.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://94.139.246.134:8080/api/v1";
+export const API_URL = 'http://94.139.246.134:8080/api/v1';
 
 export const responses = (ok, body, error) => {
     return {
