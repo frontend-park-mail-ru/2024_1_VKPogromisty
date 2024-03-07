59033427a6274d2107ef91e227b1d8d87fffa2d3
Famelbyby <lexagorbunov14@gmail.com>
2024-03-07 17:02:26 +0300

    VKP-29-validation: настроить сервер

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use("/static", express.static(path.resolve(__dirname, '..', 'static')));
 app.use("/public", express.static(path.resolve(__dirname, '..', 'public')));
+app.use("/handlebars", express.static(path.resolve(__dirname, '..', 'handlebars')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
